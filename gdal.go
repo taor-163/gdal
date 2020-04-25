344622ab5b5086d64b1c4eca57cde042140cf1fc
taor-163 <taor@163.com>
2020-04-25 23:15:09 +0800

    fix ${SRCDIR}

--- gdal.go
+++ gdal.go
@@ -6,8 +6,8 @@ package gdal
 
 #cgo linux  pkg-config: gdal
 #cgo darwin pkg-config: gdal
-#cgo windows LDFLAGS: -L${srcdir}/gdal/lib -lgdal_i
-#cgo windows CFLAGS: -I${srcdir}/gdal/include
+#cgo windows LDFLAGS: -L${SRCDIR}/gdal/lib -lgdal_i
+#cgo windows CFLAGS: -I${SRCDIR}/gdal/include
 */
 import "C"
 import (
